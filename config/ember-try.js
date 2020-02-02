ed00d998c9fa1e1f9d489deeaaa33f09759b6664
Thomas Wang <wangxgwxg@gmail.com>
2020-02-01 20:11:35 -0800

    Extend timeout and update ember version test matrix

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,23 +7,15 @@ module.exports = async function() {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.4.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.8.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.12.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.14',
         npm: {
           devDependencies: {
             'ember-source': '~3.12.0',
