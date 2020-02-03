499d07f64925b6dfe6bbdbe487960fadcaf6e3f3
Thomas Wang <wangxgwxg@gmail.com>
2020-02-02 17:39:19 -0800

    fix github actions

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,15 +7,15 @@ module.exports = async function() {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.12',
+        name: 'ember-lts-3.8',
         npm: {
           devDependencies: {
-            'ember-source': '~3.12.0',
+            'ember-source': '~3.8.0',
           },
         },
       },
       {
-        name: 'ember-lts-3.14',
+        name: 'ember-lts-3.12',
         npm: {
           devDependencies: {
             'ember-source': '~3.12.0',
